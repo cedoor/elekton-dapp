e025db1e2b8c94990a9dcb9b5473daa395afa742
cedoor <me@cedoor.dev>
2021-05-19 17:09:52 +0200

    build: add production conf parameters

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { abi as contractInterface } from "./contracts/Elekton.json"
 import { ElektonConfig } from "elekton/dist/types/types"
 
 const config: ElektonConfig = {
-    contractAddress: "0x68B1D87F95878fE05B998F19b66F4baba5De1aed",
+    contractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
     wasmFilePath: `${window.location.origin}/main.wasm`,
     zkeyFilePath: `${window.location.origin}/circuit_final.zkey`,
     contractInterface
@@ -13,8 +13,8 @@ export default {
     test: config,
     production: {
         ...config,
-        contractAddress: "",
-        ipfsProvider: "",
-        ethereumProvider: ""
+        ethereumProvider: "wss://demetra-testnet.netserv.it/uechkws",
+        ipfsProvider: "https://ipfs.infura.io:5001/api/v0",
+        contractAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
     }
 }[process.env.NODE_ENV] as ElektonConfig
